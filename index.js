a35c7e6f669a82e755eb93c7e0deda19fa67e13d
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-14 15:28:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,5 +22,9 @@ function appendDriver(name) {
 }
 
 function prependDriver(name) {
-  return [name, ...drivers]
+  return [name, ...drivers];
+}
+
+function removeLastDriver() {
+  return drivers.slice(0, drivers.length - 1)
 }
